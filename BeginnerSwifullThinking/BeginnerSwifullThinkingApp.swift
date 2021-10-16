9a255653f667c1b698d93ddb101cb6a7da0954ab
raden-dimas012 <dimascode012@gmail.com>
2021-10-16 14:22:13 +0700

    Initial Commit

---
+++ BeginnerSwifullThinking/BeginnerSwifullThinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeginnerSwifullThinkingApp.swift
+//  BeginnerSwifullThinking
+//
+//  Created by Raden Dimas on 16/10/21.
+//
+
+import SwiftUI
+
+@main
+struct BeginnerSwifullThinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
